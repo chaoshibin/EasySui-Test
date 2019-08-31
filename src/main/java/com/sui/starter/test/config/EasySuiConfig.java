1d21efb64fa15da5e66625000880996f93ed92ff
e <l>
2019-08-31 14:03:18 +0800

    mark

---
+++ src/main/java/com/sui/starter/test/config/EasySuiConfig.java
@@ -0,0 +1,17 @@
+package com.sui.starter.test.config;
+
+import com.easysui.starter.annotation.EnableEasyLock;
+import com.easysui.starter.annotation.EnableEasyLog;
+import com.easysui.starter.annotation.EnableEasyValidation;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author CHAO 2019/8/24 19:40
+ */
+@Configuration
+@EnableEasyLock
+@EnableEasyLog
+@EnableEasyValidation
+//@EnableEasyCache
+public class EasySuiConfig {
+}
---
+++ src/main/java/com/sui/starter/test/controller/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.sui.starter.test.controller;
+
+import com.easysui.web.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author chao
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()).excludePathPatterns("/css/*", "/js/*");
+    }
+}
